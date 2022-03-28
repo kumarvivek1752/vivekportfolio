5acf266c0ab9ff39ee27ec17eba667ce04d07373
vivek kumar <kumarvivek1752@gmail.com>
2022-03-28 13:00:48 +0530

    Update settings.py

--- vivekportfolio/settings.py
+++ vivekportfolio/settings.py
@@ -127,3 +127,9 @@ STATICFILES_DIRS=[
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+#heroku
+import django_heroku
+django_heroku.settings(locals())
